93b5ebc5b031bca5bb36d90bb31445f187355cdb
iVanGB93 <ivanguachbeltran@gmail.com>
2021-05-31 13:46:58 -0400

    sync

--- sorteo/admin.py
+++ sorteo/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Sorteo, SorteoDetalle
 
-admin.site.register(Sorteo)
 
 admin.site.register(SorteoDetalle)
+
+class SorteoAdminConfig(admin.ModelAdmin):
+    model = Sorteo
+    search_fields = ['usuario__username', 'code', 'mes']
+    list_filter = ('eliminado', 'mes', 'servicio', 'fecha')
+    ordering = ('-mes',)
+    list_display = ('usuario', 'servicio', 'mes', 'eliminado', 'fecha')
+    fieldsets = (
+        ('ID', {'fields': ('usuario', 'code', 'servicio', 'eliminado')}),
+        ('Fecha', {'fields': ('mes', 'fecha')}),
+    )
+
+admin.site.register(Sorteo, SorteoAdminConfig)
