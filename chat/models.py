cd7b7f490658beda6aa3a8c1ad72ce5ac21c37c2
iVanGB93 <ivanguachbeltran@gmail.com>
2021-09-10 00:04:32 -0400

    update

--- chat/models.py
+++ chat/models.py
@@ -7,6 +7,9 @@ class Mensaje(models.Model):
     contenido = models.TextField()
     fecha = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return "Mensaje: " + self.contenido + ", de: " + self.autor.username
+
 class Chat(models.Model):
     participantes = models.ManyToManyField(User)
     mensajes = models.ManyToManyField(Mensaje)
